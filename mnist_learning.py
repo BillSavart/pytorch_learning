b2b281b099a2fc275bcddc0c9b58f0d32315db33
BillSavart <energyy0512@gmail.com>
2025-04-15 16:31:02 +0800

    put some comments

--- mnist_learning.py
+++ mnist_learning.py
@@ -4,17 +4,22 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
+
+# 把資料轉成Tensor
 transform = transforms.Compose([
     transforms.ToTensor(),                # 轉為Tensor
     transforms.Normalize((0.5,), (0.5,))  # 標準化
 ])
 
+# 訓練資料=True, 測試資料=False
 train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
 test_dataset  = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
 
+# 把資料丟進去Loader, 設定Batch大小
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader  = DataLoader(test_dataset, batch_size=1000, shuffle=False)
 
+# 定義模型
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
@@ -32,9 +37,11 @@ class MLP(nn.Module):
 
 model = MLP()
 
+# 定義Loss Function 採用Cross Entropy, 以及最佳化, Learning rate = 0.001
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+# 做5個epoch
 epochs = 5
 for epoch in range(epochs):
     running_loss = 0.0
@@ -48,6 +55,7 @@ for epoch in range(epochs):
     
     print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader):.4f}")
 
+# 計算正確率
 correct = 0
 total = 0
 with torch.no_grad():
